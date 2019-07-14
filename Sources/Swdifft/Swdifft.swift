70d99984324e12782056ef3e9193134a0a719a4d
bannzai <kingkong999yhirose@gmail.com>
2019-07-14 10:33:18 +0900

    :sparkles: Add utility of string

--- Sources/Swdifft/Swdifft.swift
+++ Sources/Swdifft/Swdifft.swift
@@ -51,6 +51,25 @@ public func diff(_ lhs: Content, _ rhs: Content, formatter: Formatter = DefaultF
     return process(lhs: lhs, rhs: rhs, formatter: formatter)
 }
 
-private func process(lhs: String, rhs: String, formatter: Formatter) -> [Diff] {
+private func process(lhs: Content, rhs: Content, formatter: Formatter) -> [Diff] {
     fatalError()
+}
+
+
+internal extension String {
+    subscript(safe offset: Int) -> String.Element? {
+        return offset > count ? nil : self[offset]
+    }
+    subscript(offset: Int) -> String.Element {
+        return self[index(startIndex, offsetBy: offset)]
+    }
+    func prefix(offset: Int) -> String {
+        return String(prefix(upTo: index(startIndex, offsetBy: offset)))
+    }
+}
+
+internal extension String.Element {
+    func string() -> String {
+        return String(self)
+    }
 }
