a09cbfa863aeea1e5a259468f67bca501aaf316a
bannzai <kingkong999yhirose@gmail.com>
2019-07-14 12:01:46 +0900

    :sparkles: Fix process function

--- Sources/Swdifft/Swdifft.swift
+++ Sources/Swdifft/Swdifft.swift
@@ -12,8 +12,13 @@ public var endRHSMark = "***"
 
 public typealias Content = String
 
-public func diff(_ lhs: Content, _ rhs: Content, formatter: Formatter = DefaultFomratter()) -> [Diff] {
-    return process(lhs: lhs, rhs: rhs, formatter: formatter)
+public func diff(_ lhs: Content, _ rhs: Content, formatter: Formatter = DefaultFomratter()) -> (lhs: Content, rhs: Content) {
+    return process(
+        lhs: lhs,
+        rhs: rhs,
+        lcs: longestCommonSubsequence(lhs: lhs, rhs: rhs),
+        formatter: formatter
+    )
 }
 
 internal func longestCommonSubsequence(lhs: Content, rhs: Content) -> Content {
@@ -31,8 +36,65 @@ internal func longestCommonSubsequence(lhs: Content, rhs: Content) -> Content {
     return lcs
 }
 
-private func process(lhs: Content, rhs: Content, formatter: Formatter) -> [Diff] {
-    fatalError()
+private func process(lhs: Content, rhs: Content, lcs: Content, formatter: Formatter) -> (lhs: Content, rhs: Content) {
+    let lhsLength = lhs.count
+    let rhsLength = rhs.count
+    let maxIndex = min(lhsLength, rhsLength) - 1
+    
+    var rhsEndedIndex = 0
+    
+    struct Result {
+        var stack: String = ""
+        var beginDiff: Bool = false
+    }
+
+    func run(target: Content, commonCharacter: Character, diffWords: (begin: String, end: String)) -> Result {
+        var endedIndex = 0
+        var result = Result()
+        
+        for index in (endedIndex..<min(target.count, maxIndex)) {
+            switch commonCharacter == target[index] {
+            case false:
+                switch result.beginDiff {
+                case false:
+                    result.stack.append(contentsOf: diffWords.end)
+                    result.stack.append(target[index])
+                    result.beginDiff = true
+                case true:
+                    result.stack.append(target[index])
+                }
+            case true:
+                endedIndex = index
+                break
+            }
+            if result.beginDiff {
+                result.stack.append(contentsOf: diffWords.begin)
+                result.beginDiff = false
+            }
+        }
+        return result
+    }
+
+    let (lhsResult, rhsResult) = lcs.reduce((lhsResult: "", rhsResult: "")) { (result, commonCharacter) in
+        return (
+            result.0 + run(target: lhs, commonCharacter: commonCharacter, diffWords: (begin: beginLHSMark, end: endLHSMark)).stack,
+            result.1 + run(target: rhs, commonCharacter: commonCharacter, diffWords: (begin: beginRHSMark, end: endRHSMark)).stack
+        )
+    }
+    
+    if lhsLength == rhsLength {
+        return (lhs: lhsResult, rhs: rhsResult)
+    }
+    switch (lhsLength > rhsLength) {
+    case false:
+        let startIndex = lhs.index(lhs.startIndex, offsetBy: maxIndex)
+        let endIndex = lhs.index(startIndex, offsetBy: lhsLength - maxIndex)
+        return (lhs: lhsResult + lhs[startIndex..<endIndex], rhs: rhsResult)
+    case true:
+        let startIndex = lhs.index(lhs.startIndex, offsetBy: maxIndex)
+        let endIndex = lhs.index(startIndex, offsetBy: lhsLength - maxIndex)
+        return (lhs: lhsResult, rhs: rhsResult + rhs[startIndex..<endIndex])
+    }
 }
 
 
