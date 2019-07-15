aa31fb16d9aeb1cb2b41320df26cbec81eb6b056
bannzai <kingkong999yhirose@gmail.com>
2019-07-16 00:25:58 +0900

    :recycle: Separate files

---
+++ Sources/Swdifft/Marks.swift
@@ -0,0 +1,15 @@
+//
+//  Marks.swift
+//  Swdifft
+//
+//  Created by Yudai Hirose on 2019/07/16.
+//
+
+import Foundation
+
+public var beginLHSMark = "`"
+public var endLHSMark = "`"
+
+public var beginRHSMark = "*"
+public var endRHSMark = "*"
+
